613f439708e90a74c1d6bcac3a49e4c383c5dc38
Farok Hossain <farokafs7@gmail.com>
2024-04-21 02:05:24 +0600

    profile component added

--- app/index.jsx
+++ app/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
@@ -6,6 +7,9 @@ export default function App() {
     <View style={styles.container}>
       <Text>Aora!</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to Profile
+      </Link>
     </View>
   );
 }
