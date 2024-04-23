069732bb4a667a3c471d8072ce45062ceaa4e4cd
Farok Hossain <farokafs7@gmail.com>
2024-04-23 17:23:39 +0600

    customButton added and index updated

--- app/index.jsx
+++ app/index.jsx
@@ -8,12 +8,32 @@ export default function App() {
   return (
     <SafeAreaView className=" bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center  h-full px-4">
+        <View className="w-full  items-center  h-full px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
             resizeMode="contain"
           />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-white text-3xl font-bold text-center">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-gray-100 text-sm font-pregular mt-7 text-center">
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with Aora
+          </Text>
         </View>
       </ScrollView>
     </SafeAreaView>
