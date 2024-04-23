ebcf9f0e13e8de4c03166238585a6c86591b4989
Farok Hossain <farokafs7@gmail.com>
2024-04-23 12:29:19 +0600

    index updated

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -14,6 +14,7 @@ const TabIcon = ({ icon, color, name, focused }) => {
       />
       <Text
         className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color: color }}
       >
         {name}
       </Text>
@@ -27,6 +28,14 @@ const TabsLayout = () => {
       <Tabs
         screenOptions={{
           tabBarShowLabel: false,
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarStyle: {
+            backgroundColor: "#161622",
+            borderTopWidth: 1,
+            borderTopColor: "#232533",
+            height: 84,
+          },
         }}
       >
         <Tabs.Screen
--- app/index.jsx
+++ app/index.jsx
@@ -1,18 +1,29 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Aora!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: "blue" }}>
-        Go to Home
-      </Link>
-      <Link href="/sign-in" style={{ color: "blue" }}>
-        Go to Signin
-      </Link>
-    </View>
+    <SafeAreaView className=" bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center  h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
+}
+
+{
+  /* <Text className="text-3xl font-pblack">Aora!</Text>
+<StatusBar style="auto" />
+<Link href="/home" style={{ color: "blue" }}>
+  Go to Home
+</Link> */
 }
