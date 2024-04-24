e682c1cb6c26554fba64cd28e70b5cc254c54964
Farok Hossain <farokafs7@gmail.com>
2024-04-24 12:18:54 +0600

    updated

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,5 +1,6 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 import React, { useState } from "react";
+import { icons } from "../constants";
 
 const FormField = ({
   title,
@@ -14,7 +15,7 @@ const FormField = ({
   return (
     <View className="space-y-2 mt-7">
       <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
-      <View className=" w-full h-16 px-4 bg-black-100 border-2 border-black-100  rounded-2xl focus:border-secondary items-center">
+      <View className=" w-full h-16 px-4 bg-black-100 border-2 border-black-100  rounded-2xl focus:border-secondary items-center flex-row">
         <TextInput
           className="flex-1 text-white font-psemibold text-base"
           value={value}
@@ -23,6 +24,16 @@ const FormField = ({
           onChangeText={handleChangeText}
           secureTextEntry={title === "Password" && !showPassword}
         />
+
+        {title === "Password" && (
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Image
+              source={!showPassword ? icons.eye : icons.eyeHide}
+              className="w-6 h-6"
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        )}
       </View>
     </View>
   );
