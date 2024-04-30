e52313b414a92a3dc6febaffe458bbe78c49511e
Farok Hossain <farokafs7@gmail.com>
2024-04-30 23:52:26 +0600

    sign up updated

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -19,7 +19,7 @@ const SignIn = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center h-full  px-4 my-6 ">
+        <View className="w-full justify-center min-h-[83vh] px-4 my-6 ">
           <Image
             source={images.logo}
             resizeMode="contain"
