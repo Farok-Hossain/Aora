e337b5143f70e9e8cfc18a9096fe260219047cd0
Farok Hossain <farokafs7@gmail.com>
2024-05-02 15:33:21 +0600

    updated

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -5,6 +5,7 @@ import { images } from "../../constants";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
 import { Link } from "expo-router";
+import { createUser } from "../../lib/appwrite";
 
 const SignUp = () => {
   const [form, setForm] = useState({
@@ -15,7 +16,9 @@ const SignUp = () => {
 
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const submit = () => {};
+  const submit = () => {
+    createUser();
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full">
