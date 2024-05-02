1b7fbb6e931df8b1aed88f935628e470c54431ec
Farok Hossain <farokafs7@gmail.com>
2024-05-02 16:18:00 +0600

    context provider created

---
+++ context/GlobalProvider.js
@@ -0,0 +1,12 @@
+import { createContext, useContext, useState } from "react";
+
+const GlobalContext = createContext();
+
+export const useGlobalContext = () => useContext(GlobalContext);
+
+const GlobalProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
+};
