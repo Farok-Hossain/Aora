6f0c8e19995958c7818e05fb41311e32eac7c710
Farok Hossain <farokafs7@gmail.com>
2024-05-05 16:08:11 +0600

    home updated

--- components/EmptyState.jsx
+++ components/EmptyState.jsx
@@ -1,20 +1,26 @@
 import { View, Text, Image } from "react-native";
 import React from "react";
 import { images } from "../constants";
+import { router } from "expo-router";
 
 const EmptyState = ({ title, subtitle }) => {
   return (
     <View className="justify-center items-center px-4">
       <Image
         source={images.empty}
-        className=" w-72 h-56"
+        className=" w-[270px] h-[215px]"
         resizeMode="contain"
       />
-
-      <Text className=" font-pmedium text-sm text-gray-100">{title}</Text>
       <Text className="text-xl text-white text-center font-psemibold mt-2">
-        {subtitle}
+        {title}
       </Text>
+      <Text className=" font-pmedium text-sm text-gray-100">{subtitle}</Text>
+
+      {/* <CustomButton
+        title="Create video"
+        handlePress={() => router.push("/create")}
+        containerStyles="w-full my-5"
+      /> */}
     </View>
   );
 };
