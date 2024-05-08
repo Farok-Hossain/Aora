11729c2615f7081dfbd592b6016e62c86b8b8c94
Farok Hossain <farokafs7@gmail.com>
2024-05-08 21:09:34 +0600

    updated videoCard

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -43,13 +43,22 @@ const VideoCard = ({
         </View>
       </View>
       {play ? (
-        <Text>Playing</Text>
+        <Text className="text-white">Playing</Text>
       ) : (
-        <TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={() => setPlay(true)}
+          className="w-full h-60 rounded-lg mt-3 relative justify-center items-center"
+        >
           <Image
             source={{ uri: thumbnail }}
             className="w-full h-full rounded-xl mt-3"
             resizeMode="cover"
+          />
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
           />
         </TouchableOpacity>
       )}
