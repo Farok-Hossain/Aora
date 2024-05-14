0d7969a07e7c322f5f35ead8e4396d21cf4b8e65
Farok Hossain <farokafs7@gmail.com>
2024-05-14 15:58:57 +0600

    updated search

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,14 +1,65 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import { View, Text, FlatList, Image, RefreshControl } from "react-native";
+import { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../../constants";
+import SearchInput from "../../components/SearchInput";
+import Trending from "../../components/Trending";
+import EmptyState from "../../components/EmptyState";
+import useAppwrite from "../../lib/useAppwrite";
+import { searchPosts } from "../../lib/appwrite";
+import VideoCard from "../../components/VideoCard";
+import { useLocalSearchParams } from "expo-router";
 
 const Search = () => {
   const { query } = useLocalSearchParams();
+  const { data: posts, refetch } = useAppwrite(searchPosts(query));
+
+  useEffect(() => {
+    refetch();
+  }, [query]);
 
   return (
     <SafeAreaView className="bg-primary h-full">
-      <Text className=" text-3xl text-white">{query}</Text>
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-psemibold text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">Aora</Text>
+              </View>
+              <View className="mt-1.5">
+                <Image
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                  source={images.logoSmall}
+                />
+              </View>
+            </View>
+            <SearchInput />
+
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-gray-100 text-lg font-pregular mb-3">
+                Latest Videos
+              </Text>
+
+              <Trending posts={latestPosts ?? []} />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="Be the first one to upload a video"
+          />
+        )}
+      />
     </SafeAreaView>
   );
 };
