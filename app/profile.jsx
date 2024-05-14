d31d25fae9ff4de82f32f2aa267670c714ea774b
Farok Hossain <farokafs7@gmail.com>
2024-05-14 20:00:12 +0600

    profile updated

--- app/profile.jsx
+++ app/profile.jsx
@@ -5,17 +5,13 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import SearchInput from "../../components/SearchInput";
 import EmptyState from "../../components/EmptyState";
 import useAppwrite from "../../lib/useAppwrite";
-import { searchPosts } from "../../lib/appwrite";
 import VideoCard from "../../components/VideoCard";
-import { useLocalSearchParams } from "expo-router";
+import { getUserPosts } from "../lib/appwrite";
+import { useGlobalContext } from "../context/GlobalProvider";
 
 const Profile = () => {
-  const { query } = useLocalSearchParams();
-  const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
-
-  useEffect(() => {
-    refetch();
-  }, [query]);
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
+  const { data: posts } = useAppwrite(() => getUserPosts(user.$id));
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -23,17 +19,7 @@ const Profile = () => {
         data={posts}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => <VideoCard video={item} />}
-        ListHeaderComponent={() => (
-          <View className="my-6 px-4">
-            <Text className="font-psemibold text-sm text-gray-100">
-              Search Results
-            </Text>
-            <Text className="text-2xl font-psemibold text-white">{query}</Text>
-            <View className="mt-6 mb-8">
-              <SearchInput initialQuery={query} />
-            </View>
-          </View>
-        )}
+        ListHeaderComponent={() => <View></View>}
         ListEmptyComponent={() => (
           <EmptyState
             title="No Videos Found"
