940fa6c23b8f68af018ebb4329b134bb8c08b3ae
Farok Hossain <farokafs7@gmail.com>
2024-05-21 21:57:41 +0600

    profile updated

--- app/profile.jsx
+++ app/profile.jsx
@@ -14,6 +14,8 @@ const Profile = () => {
   const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const { data: posts } = useAppwrite(() => getUserPosts(user.$id));
 
+  const logout = () => {};
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList
@@ -22,13 +24,23 @@ const Profile = () => {
         renderItem={({ item }) => <VideoCard video={item} />}
         ListHeaderComponent={() => (
           <View className="w-full justify-center items-center mt-6  mb-12 px-4">
-            <TouchableOpacity>
+            <TouchableOpacity
+              className="w-full items-end mb-10"
+              onPress={logout}
+            >
               <Image
                 source={icons.logout}
                 resizeMode="contain"
                 className="w-6 h-6"
               />
             </TouchableOpacity>
+            <View className="w-16 h-16 border border-secondary rounded-lg justify-center items-center">
+              <Image
+                source={{ uri: user?.avatar }}
+                className="w-[90%] h-[90%] rounded-lg"
+                resizeMode="cover"
+              />
+            </View>
           </View>
         )}
         ListEmptyComponent={() => (
