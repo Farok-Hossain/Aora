9b9bfc6c3ff9c34ad34755ca836211a12b610d27
Farok Hossain <farokafs7@gmail.com>
2024-05-22 22:21:32 +0600

    profile updated

--- app/profile.jsx
+++ app/profile.jsx
@@ -4,16 +4,23 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import EmptyState from "../../components/EmptyState";
 import useAppwrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
-import { getUserPosts } from "../lib/appwrite";
+import { getUserPosts, signOut } from "../lib/appwrite";
 import { useGlobalContext } from "../context/GlobalProvider";
 import icons from "../constants";
 import InfoBox from "../components/InfoBox";
+import { router } from "expo-router";
 
 const Profile = () => {
   const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const { data: posts } = useAppwrite(() => getUserPosts(user.$id));
 
-  const logout = () => {};
+  const logout = async () => {
+    await signOut();
+    setUser(null);
+    setIsLoggedIn(false);
+
+    router.replace("/sign-in");
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full">
