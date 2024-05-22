90199b12b28c5d17943f4e0959e9ae8447369d08
Farok Hossain <farokafs7@gmail.com>
2024-05-22 21:57:55 +0600

    updated

--- components/InfoBox.jsx
+++ components/InfoBox.jsx
@@ -7,6 +7,9 @@ const InfoBox = ({ title, subtitle, containerStyles, titleStyles }) => {
       <Text className={`text-white text-center font-psemibold ${titleStyles}`}>
         {title}
       </Text>
+      <Text className="text-sm text-gray-100 text-center font-pregular">
+        {subtitle}
+      </Text>
     </View>
   );
 };
