50173b41e61e6c3a122aae4750843a83896f82f5
Farok Hossain <farokafs7@gmail.com>
2024-05-23 12:56:28 +0600

    updated

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -33,7 +33,13 @@ const Create = () => {
           </Text>
           <TouchableOpacity>
             {form.video ? (
-              <Video />
+              <Video
+                source={{ uri: form.video.uri }}
+                className=" w-full h-64 rounded-2xl"
+                useNativeControls
+                resizeMode={ResizeMode.COVER}
+                isLooping
+              />
             ) : (
               <View className="w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center">
                 <View className="w-14 h-14 border border-dashed border-secondary-100 justify-center items-center">
@@ -43,6 +49,28 @@ const Create = () => {
                     className="w-1/2 h-1/2"
                   />
                 </View>
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
+        <View className="mt-7 space-y-2">
+          <Text className="text-base text-gray-100 font-pmedium">
+            Thumbnail Image
+          </Text>
+          <TouchableOpacity>
+            {form.thumbnail ? (
+              <Image
+                source={{ uri: form.thumbnail.uri }}
+                resizeMode="cover"
+                className="w-full h-64 rounded-2xl"
+              />
+            ) : (
+              <View className="w-full h-16 px-4 bg-black-100 rounded-2xl justify-center items-center border-2 border-black-200 flex-row space-x-2">
+                <Image
+                  source={icons.upload}
+                  resizeMode="contain"
+                  className="w-5 h-5"
+                />
               </View>
             )}
           </TouchableOpacity>
