9ed1ef98a9f8f465661bbb29ee894cbdbce082c2
Farok Hossain <farokafs7@gmail.com>
2024-05-23 12:35:35 +0600

    updated

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,8 +1,17 @@
 import { Text, ScrollView } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import FormField from "../../components/FormField";
 
 const Create = () => {
+  const [uploading, setUploading] = useState(false);
+  const [form, setForm] = useState({
+    title: "",
+    video: null,
+    thumbnail: null,
+    prompt: "",
+  });
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView className="px-4 my-6">
